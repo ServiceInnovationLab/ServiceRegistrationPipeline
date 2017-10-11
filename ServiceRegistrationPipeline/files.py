dd2a19997611a32f8ac2dc3a0ee4f4945d82343a
Brenda Wallace <brenda@wallace.net.nz>
2017-10-12 11:04:26 +1300

    pep8 tidy up

--- ServiceRegistrationPipeline/files.py
+++ ServiceRegistrationPipeline/files.py
@@ -1,4 +1,6 @@
 from os import listdir
+
+
 def find_files(files_dir):
-  files = [f for f in listdir(files_dir) if isfile(
+    files = [f for f in listdir(files_dir) if isfile(
         join(files_dir, f))]
