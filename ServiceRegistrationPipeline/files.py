a6b310b6cf7fdbf575146b47c16c09dcb8740f12
Brenda Wallace <brenda@wallace.net.nz>
2017-10-13 12:13:49 +1300

    Fixed imports in file finder

--- ServiceRegistrationPipeline/files.py
+++ ServiceRegistrationPipeline/files.py
@@ -1,6 +1,8 @@
 from os import listdir
+from os.path import isfile, join
 
 
 def find_files(files_dir):
     files = [f for f in listdir(files_dir) if isfile(
         join(files_dir, f))]
+    return files
